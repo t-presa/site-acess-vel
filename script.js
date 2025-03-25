0d60110a875db71fea334a94c1e967d0756027ad
t-presa <159184713+t-presa@users.noreply.github.com>
2025-03-25 10:55:11 -0300

    trabalho dia 18 de março

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    
+    let tamanhoAtualFont= 1;
+    
+    aumentaFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFont + = 0,1;
+    document.body.style.fontSize = '$(tamanhoAtualFonte)rem'
+    }
+    )
+    }
+    )
