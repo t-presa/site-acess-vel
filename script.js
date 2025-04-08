50c55be5dd5a95ea88e24323e374b340c49eea88
t-presa <159184713+t-presa@users.noreply.github.com>
2025-04-08 11:15:34 -0300

    trabalho dia 08 de Abril de 2025

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
-    const botaoAcessibildade = document.getElementById('botao-acessibilidade');
-    const opcoesAcessibilidade = document.getElementById('opcoes-acessibilidade');
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
-    botaoAcessibilidade.addEventListener('click, function'();
-    botaoAcessibilidade.classList.toggle('rotacao-botao');
-    opcoesAcessibilidade.classList.toggle('apresenta-lista');
-
-    )
+    })
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-    let tamanhoAtualFonte= 1;
+
+    const alternaContraste = document.getElementById('alterna-contraste')
+
+    let tamanhoAtualFonte = 1;
+
     aumentaFonteBotao.addEventListener('click', function () {
-    tamanhoAtualFonte += 0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
     })
+
     diminuiFonteBotao.addEventListener('click', function () {
-    tamanhoAtualFonte -= 0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
     })
+
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
     })
+
+
+})
